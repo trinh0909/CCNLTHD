ae3767fd85c6b6257243a5932a4c57e2f1813363
trinh0909 <truongmaitrinh0909@gmail.com>
2024-11-12 11:02:25 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// const { render } = require("ejs");
+// const express = require("express"); // Thêm dòng này để khai báo express
+// const app = express();
+
+// app.set("view engine","ejs")
+// app.set("view","./image")
+
+// const port = 3000;
+
+// app.get("/", (req,res)=>{
+//     res.render("login.ejs")
+// })
+
+// app.listen(port, () => console.log("Ứng dụng đang chạy trên port 3000"));
+
+const express = require("express"); 
+const app = express();
+
+app.set("view engine", "ejs");
+// app.set("views", "./views"); // Đảm bảo có thư mục "views" trong thư mục dự án của bạn
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.render("login"); // Không cần thêm .ejs
+});
+
+app.listen(port, () => console.log("Ứng dụng đang chạy trên port 3000"));
